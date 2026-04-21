454343fde1c566b83e71ed0eaac8b7eaa6bcf3b3
zana025 <zana.b_04@hotmail.com>
2026-04-21 14:17:34 +0200

    Lagt till Router och simpel Navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,14 +2,23 @@ import "./App.css"
 import Startpage from "./components/Startpage"
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query"
 import TestShop from "./dev/TestShop"
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom"
 
 const queryClient = new QueryClient()
 
 const App = () => {
   return (
     <QueryClientProvider client={queryClient}>
-      {/* TestShop ska bytas ut mot startpage */}
-      <Startpage></Startpage>
+      <Router>
+        <nav>
+          <Link to="/">Start</Link>
+          <Link to="/kundvagn">Kundvagn</Link>
+        </nav>
+
+        <Routes>
+          <Route path="/" element={<Startpage />} />
+        </Routes>
+      </Router>
     </QueryClientProvider>
   )
 }

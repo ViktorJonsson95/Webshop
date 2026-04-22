6cfdc190ebb77da4b9b09bbeabff8e0d2c0a40e0
Kim Karlsson <kimelliotkarlsson@gmail.com>
2026-04-22 19:20:10 +0200

    Implement ProductPage and add react-router-dom dependency

--- frontend/src/components/ProductPage.jsx
+++ frontend/src/components/ProductPage.jsx
@@ -1,28 +1,21 @@
-import { useQuery } from '@tanstack/react-query';
-
+import { useProduct } from '../hooks/useProduct';
+import { useParams } from 'react-router-dom';
 
 
 export default function ProductPage() {
-    const { data: product, isLoading, isError } = useQuery({
-        queryKey: ['product'],
-        queryFn: () => { // Mock-data tills backend/API är klart
-            return {
-                name: 'Testprodukt',
-                price: 199.00,
-                description: 'Beskrivning av testprodukt'
-            }
-        }
-    })
+    const { id } = useParams();
+    const { data, isLoading, isError } = useProduct(id);
 
     if (isLoading) return <p>Loading, please wait...</p>
-    if (isError) return <p>Something went wrong!</p>
+    //Om det blev fel ELLER om data är tom, visa felmeddelande
+    if (isError || !data ) return <p>Something went wrong! Couldn't find product.</p>
 
     return (
         <div>
             <h1>Product Page</h1>
-            <h2>{product.name}</h2>
-            <p>{product.price}</p>
-            <p>{product.description}</p>
+            <h2>{data.name}</h2>
+            <p>{data.price}</p>
+            <p>{data.description}</p>
         </div>
     )
 }

75747b51e6c1ab7d0563e4897b53d7728cce2d13
zana025 <zana.b_04@hotmail.com>
2026-05-06 15:08:46 +0200

    kommentarer
    kommenterade i navbar och i modal

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -7,15 +7,20 @@ import { useEffect } from "react"
 import useCart from "../hooks/useCart"
 
 export default function Navbar() {
+  //aktuell route/URL, kundvagn är öppen eller stängd
   const location = useLocation()
   const [cartOpen, setCartOpen] = useState(false)
+
+  //kollar om vi är i checkout
   const isCheckout = location.pathname === "/checkout"
+
+  //hämtar cart items från hooken, om undifined så sätt till tom array
   const { data: cartItems = [] } = useCart()
 
   //Räkna antalet produkter i cart
   const totalQuantity = cartItems.reduce(
     (sum, item) => sum + (item.quantity ?? 1),
-    0
+    0,
   )
   //Om vi är i checkout så stängs navbar
   useEffect(() => {
@@ -23,18 +28,20 @@ export default function Navbar() {
   }, [location.pathname])
 
   return (
+    // navbar som ligger kvar högst upp vid scroll
     <header className="sticky top-0 z-50 bg-blue-100 border-b border-slate-800">
+      {/*container som håller innehållet centrerat  */}
       <div className="max-w-7xl mx-auto px-6 h-16 flex items-center justify-between">
+        {/*logga som länkar tillbaka till startsida  */}
         <Link to="/">
-          <h1 className="text-2xl font-bold tracking-wider">
-            Runova
-          </h1>
+          <h1 className="text-2xl font-bold tracking-wider">Runova</h1>
         </Link>
-
+        {/*visa inte kundvagnsknappen på checkpout sidan */}
         {!isCheckout && (
+          //knapp som öppnar kundvagn
           <button onClick={() => setCartOpen(true)} className="relative">
             <FaShoppingCart />
-
+            {/*visar badge med antal produkter i kundvagnen om det finns några */}
             {totalQuantity > 0 && (
               <span className="absolute -top-2 -right-2 bg-red-500 text-white text-xs rounded-full px-2">
                 {totalQuantity}
@@ -43,7 +50,7 @@ export default function Navbar() {
           </button>
         )}
 
-
+        {/*Modal som visar kundvagnen när cartOpen är true, och stängs när onClose anropas */}
         <Modal open={cartOpen} onClose={() => setCartOpen(false)}>
           <ShoppingCart />
         </Modal>
